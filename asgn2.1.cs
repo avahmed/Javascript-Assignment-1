2b4e874b2db69409a6725e3f7582bbbbde2c3ee1
avahmed <avahmed@deloitte.com>
2024-09-04 18:15:01 +0530

    Assignment 2

---
+++ asgn2.1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class asgn2.1
+    {
+        static void Main(string[] args)
+        {
+            int eid;
+            string ename;
+            string ejob;
+            string sal;
+
+            Console.WriteLine("Enter ID: ");
+            eid=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Employee Name: ");
+            ename = (Console.ReadLine());
+
+            Console.WriteLine("Enter Job: ");
+            ejob = (Console.ReadLine());
+
+            Console.WriteLine("Enter Salary: ");
+            sal = (Console.ReadLine());
+
+            Console.WriteLine("~~~~~~ Employee Details ~~~~~~");
+            Console.WriteLine("Emp Id: "+eid);
+            Console.WriteLine("Emp Name: " + ename);
+            Console.WriteLine("Job: " + ejob);
+            Console.WriteLine("Salary: " + sal);
+
+            Console.ReadLine();
+        }
+    }
+}
